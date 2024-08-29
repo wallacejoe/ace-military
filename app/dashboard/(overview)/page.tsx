8c6274c4401839fa2de7881c4ec84b6d2ea17725
wallacejoe <JAWcoder@gmail.com>
2024-08-29 09:44:57 -0700

    removed empty className lines. Added a space to the end of the dashboard page.tsx file

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,29 +1,26 @@
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Home',
+  title: "Home",
 };
 
 export default async function Page() {
-
-    return (
-        <main className=''>
-            <div className='flex flex-col items-center gap-5 m-20'>
-                <section className='w-10/12 border-2 border-black text-center bg-gray-500 text-white px-5 py-5'>
-                    <h2 className=''>
-                        New Orders
-                    </h2>
-                    <p>Header and link to full message</p>
-                </section>
-                <section className='w-10/12 border-2 border-black text-center bg-gray-500 text-white px-5 py-5'>
-                    <h2>High Priority Message</h2>
-                    <p>Header and link to full message</p>
-                </section>
-                <section className='w-10/12 border-2 border-black text-center bg-gray-500 text-white px-5 py-5'>
-                    <h2>High Priority Message</h2>
-                    <p>Header and link to full message</p>
-                </section>
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <div className="flex flex-col items-center gap-5 m-20">
+        <section className="w-10/12 border-2 border-black text-center bg-gray-500 text-white px-5 py-5">
+          <h2>New Orders</h2>
+          <p>Header and link to full message</p>
+        </section>
+        <section className="w-10/12 border-2 border-black text-center bg-gray-500 text-white px-5 py-5">
+          <h2>High Priority Message</h2>
+          <p>Header and link to full message</p>
+        </section>
+        <section className="w-10/12 border-2 border-black text-center bg-gray-500 text-white px-5 py-5">
+          <h2>High Priority Message</h2>
+          <p>Header and link to full message</p>
+        </section>
+      </div>
+    </main>
+  );
 }
