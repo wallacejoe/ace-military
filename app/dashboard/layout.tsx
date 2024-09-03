1fbc1a2c6705cde02031c37e3dd4473958c92c81
wallacejoe <JAWcoder@gmail.com>
2024-09-03 07:28:46 -0700

    Removed unused div

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,9 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     // applies to all pages in dashboard directory
     <div>
-      <div className="">
-        <Header />
-      </div>
+      <Header />
       <div>{children}</div>
     </div>
   );
