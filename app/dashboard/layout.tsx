984a8da992fc3d40851b7af74735c3fa015bcfed
wallacejoe <JAWcoder@gmail.com>
2024-09-04 07:56:21 -0700

    Fixed React component default exports

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,12 @@
 import Header from "@/app/components/header/page";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    // applies to all pages in dashboard directory
     <div>
       <Header />
       <div>{children}</div>
     </div>
   );
 }
+
+export default Layout
