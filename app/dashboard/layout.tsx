57f683416a5355544f65db1af23c0aac9b47a1c3
wallacejoe <JAWcoder@gmail.com>
2025-03-11 19:04:40 -0700

    Made some color changes to layout, header, footer

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Header />
-      <div className="flex w-full">
+      <div className="flex w-full bg-gray-200">
         <Sidenav />
         <div className="w-full">{children}</div>
       </div>
